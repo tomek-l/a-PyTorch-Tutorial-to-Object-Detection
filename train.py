db3268d4349289d7f1befc5870c7d7a382e57a01
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-08-30 20:43:39 -0700

    updated train.py with black

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from datasets import PascalVOCDataset
 from utils import *
 
 # Data parameters
-data_folder = './'  # folder with data files
+data_folder = "./data/"  # folder with data files
 keep_difficult = True  # use objects considered difficult to detect?
 
 # Model parameters
@@ -46,31 +46,41 @@ def main():
         not_biases = list()
         for param_name, param in model.named_parameters():
             if param.requires_grad:
-                if param_name.endswith('.bias'):
+                if param_name.endswith(".bias"):
                     biases.append(param)
                 else:
                     not_biases.append(param)
-        optimizer = torch.optim.SGD(params=[{'params': biases, 'lr': 2 * lr}, {'params': not_biases}],
-                                    lr=lr, momentum=momentum, weight_decay=weight_decay)
+        optimizer = torch.optim.SGD(
+            params=[{"params": biases, "lr": 2 * lr}, {"params": not_biases}],
+            lr=lr,
+            momentum=momentum,
+            weight_decay=weight_decay,
+        )
 
     else:
         checkpoint = torch.load(checkpoint)
-        start_epoch = checkpoint['epoch'] + 1
-        print('\nLoaded checkpoint from epoch %d.\n' % start_epoch)
-        model = checkpoint['model']
-        optimizer = checkpoint['optimizer']
+        start_epoch = checkpoint["epoch"] + 1
+        print("\nLoaded checkpoint from epoch %d.\n" % start_epoch)
+        model = checkpoint["model"]
+        optimizer = checkpoint["optimizer"]
 
     # Move to default device
     model = model.to(device)
     criterion = MultiBoxLoss(priors_cxcy=model.priors_cxcy).to(device)
 
     # Custom dataloaders
-    train_dataset = PascalVOCDataset(data_folder,
-                                     split='train',
-                                     keep_difficult=keep_difficult)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                                               collate_fn=train_dataset.collate_fn, num_workers=workers,
-                                               pin_memory=True)  # note that we're passing the collate function here
+    train_dataset = PascalVOCDataset(
+        data_folder, split="train", keep_difficult=keep_difficult
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=train_dataset.collate_fn,
+        num_workers=workers,
+        pin_memory=True,
+    )  # note that we're passing the collate function here
 
     # Calculate total number of epochs to train and the epochs to decay learning rate at (i.e. convert iterations to epochs)
     # To convert iterations to epochs, divide iterations by the number of iterations per epoch
@@ -86,11 +96,13 @@ def main():
             adjust_learning_rate(optimizer, decay_lr_to)
 
         # One epoch's training
-        train(train_loader=train_loader,
-              model=model,
-              criterion=criterion,
-              optimizer=optimizer,
-              epoch=epoch)
+        train(
+            train_loader=train_loader,
+            model=model,
+            criterion=criterion,
+            optimizer=optimizer,
+            epoch=epoch,
+        )
 
         # Save checkpoint
         save_checkpoint(epoch, model, optimizer)
@@ -124,7 +136,9 @@ def train(train_loader, model, criterion, optimizer, epoch):
         labels = [l.to(device) for l in labels]
 
         # Forward prop.
-        predicted_locs, predicted_scores = model(images)  # (N, 8732, 4), (N, 8732, n_classes)
+        predicted_locs, predicted_scores = model(
+            images
+        )  # (N, 8732, 4), (N, 8732, n_classes)
 
         # Loss
         loss = criterion(predicted_locs, predicted_scores, boxes, labels)  # scalar
@@ -147,14 +161,27 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         # Print status
         if i % print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, i, len(train_loader),
-                                                                  batch_time=batch_time,
-                                                                  data_time=data_time, loss=losses))
-    del predicted_locs, predicted_scores, images, boxes, labels  # free some memory since their histories may be stored
+            print(
+                "Epoch: [{0}][{1}/{2}]\t"
+                "Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t"
+                "Data Time {data_time.val:.3f} ({data_time.avg:.3f})\t"
+                "Loss {loss.val:.4f} ({loss.avg:.4f})\t".format(
+                    epoch,
+                    i,
+                    len(train_loader),
+                    batch_time=batch_time,
+                    data_time=data_time,
+                    loss=losses,
+                )
+            )
+    del (
+        predicted_locs,
+        predicted_scores,
+        images,
+        boxes,
+        labels,
+    )  # free some memory since their histories may be stored
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
